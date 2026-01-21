e62f7e91ff7da38a2a5b4b39888c70fade8d8c6b
Kashif Ali <136927508+Kashifalii@users.noreply.github.com>
2026-01-21 17:02:15 +0500

    homepage completed

--- Components/swiper/page.tsx
+++ Components/swiper/page.tsx
@@ -1,29 +1,64 @@
 "use client";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
+import {
+  ChevronRight,
+  CircleArrowRight,
+  CircleChevronLeft,
+  CircleChevronRight,
+} from "lucide-react";
 
 import "swiper/css";
-import "swiper/css/navigation";
 
 import HomePage from "../../pages/home";
 
 export default function SwiperComp() {
   const [activeIndex, setActiveIndex] = useState(0);
 
-  return (
-    <Swiper
-      navigation
-      modules={[Navigation]}
-      onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
-      className="mySwiper"
-    >
-      <SwiperSlide>
-        <HomePage isActive={activeIndex === 0} />
-      </SwiperSlide>
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
 
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-    </Swiper>
+  return (
+    <div className="relative">
+      {/* Custom Navigation */}
+      <button ref={prevRef} className="absolute right-68 bottom-10 z-50 ">
+        <CircleChevronLeft
+          strokeWidth={1.2}
+          size={48}
+          className="text-white cursor-pointer font-light"
+        />
+      </button>
+
+      <button ref={nextRef} className="absolute  right-50 bottom-10 z-50 ">
+        <CircleChevronRight
+          strokeWidth={1.2}
+          size={48}
+          className="text-white cursor-pointer font-light"
+        />
+      </button>
+
+      <Swiper
+        speed={1000}
+        modules={[Navigation]}
+        onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+        navigation={{
+          prevEl: prevRef.current,
+          nextEl: nextRef.current,
+        }}
+        onBeforeInit={(swiper) => {
+          (swiper.params.navigation as any).prevEl = prevRef.current;
+          (swiper.params.navigation as any).nextEl = nextRef.current;
+        }}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <HomePage isActive={activeIndex === 0} />
+        </SwiperSlide>
+
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+      </Swiper>
+    </div>
   );
 }
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
 import React from "react";
 
-import clock from "@/public/Images/c1.webp";
 import { MoveRight } from "lucide-react";
 import { motion } from "framer-motion";
 
@@ -9,91 +7,59 @@ function HomePage({ isActive }: { isActive: boolean }) {
   return (
     <section className="w-full h-screen bg-[linear-gradient(145deg,rgba(39,39,39,0.7)_50%,rgba(217,70,96,0.94)_50%)] overflow-hidden">
       <div className="container mx-auto px-4">
-        <Image
+        <motion.img
+          initial={{ scale: 0, rotate: -180, opacity: 0 }}
+          animate={
+            isActive
+              ? { scale: 1, rotate: 0, opacity: 1 }
+              : { scale: 0, rotate: -180, opacity: 0 }
+          }
+          transition={{ duration: 1.1 }}
           alt="Clock Image"
-          src={clock}
+          src="/Images/c1.webp"
           className="w-187 h-187 absolute z-10 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover"
-        ></Image>
+        />
         {/* text-wrapper */}
         <div className="h-screen w-full flex items-center">
           {/* left */}
-          <div className="w-1/2 h-auto">
-            <motion.p
-              initial={{ x: -500, opacity: 0 }}
-              animate={
-                isActive ? { x: 0, opacity: 1 } : { x: -500, opacity: 0 }
-              }
-              transition={{
-                delay: 0.3,
-                duration: 3,
-                type: "spring",
-                stiffness: 50,
-              }}
-              className="text-red uppercase font-sans font-medium text-[26px]"
-            >
+          <motion.div
+            initial={{ x: -500, opacity: 0 }}
+            animate={isActive ? { x: 0, opacity: 1 } : { x: -500, opacity: 0 }}
+            transition={{ duration: 1.2 }}
+            className="w-1/2 h-auto"
+          >
+            <p className="text-red uppercase font-sans font-medium text-[26px]">
               Timeless Precision- Modern Design
-            </motion.p>
-            <motion.h1
-              initial={{ x: -500, opacity: 0 }}
-              animate={
-                isActive ? { x: 0, opacity: 1 } : { x: -500, opacity: 0 }
-              }
-              transition={{ duration: 3, type: "spring", stiffness: 50 }}
-              className="text-[243px] font-oswald font-semibold text-white leading-48 ps"
-            >
+            </p>
+            <h1 className="text-[243px] font-oswald font-semibold text-white leading-48 ps">
               SMART
-            </motion.h1>
-            <motion.h3
-              initial={{ x: -500, opacity: 0 }}
-              animate={
-                isActive ? { x: 0, opacity: 1 } : { x: -500, opacity: 0 }
-              }
-              transition={{
-                delay: 0.2,
-                duration: 3,
-                type: "spring",
-                stiffness: 50,
-              }}
-              className="text-[85px] font-oswald font-extralight text-white"
-            >
+            </h1>
+            <h3 className="text-[85px] font-oswald font-extralight text-white">
               ChronoX - Series
-            </motion.h3>
-          </div>
+            </h3>
+          </motion.div>
           {/* right */}
-          <div className="w-1/2 h-auto flex flex-col items-end translate-y-12">
-            <motion.h1
-              initial={{ x: 500, opacity: 0 }}
-              animate={isActive ? { x: 0, opacity: 1 } : { x: 500, opacity: 0 }}
-              transition={{ duration: 3, type: "spring", stiffness: 50 }}
-              className="text-[243px] font-oswald font-semibold text-black leading-48 ps"
-            >
+          <motion.div
+            initial={{ x: 500, opacity: 0 }}
+            animate={isActive ? { x: 0, opacity: 1 } : { x: 500, opacity: 0 }}
+            transition={{ duration: 1.2 }}
+            className="w-1/2 h-auto flex flex-col items-end translate-y-12"
+          >
+            <h1 className="text-[243px] font-oswald font-semibold text-black leading-48 ps">
               WATCH
-            </motion.h1>
-            <motion.p
-              initial={{ x: 500, opacity: 0 }}
-              animate={isActive ? { x: 0, opacity: 1 } : { x: 500, opacity: 0 }}
-              transition={{ duration: 3, type: "spring", stiffness: 50 }}
-              className="max-w-[451px]  w-full text-xl font-sans font-light text-white mt-8 text-end"
-            >
+            </h1>
+            <p className="max-w-[451px]  w-full text-xl font-sans font-light text-white mt-8 text-end">
               Discover watches crafted for those who value elegance,
               performance, and lasting quality. Designed to elevate every
               moment.
-            </motion.p>
-            <motion.a
-              initial={{ x: 500, opacity: 0 }}
-              animate={isActive ? { x: 0, opacity: 1 } : { x: 500, opacity: 0 }}
-              transition={{
-                delay: 0.2,
-                duration: 3,
-                type: "spring",
-                stiffness: 50,
-              }}
+            </p>
+            <a
               href={"#"}
               className="text-[42px] font-oswald font-semibold text-white"
             >
               Order Now <MoveRight className="inline size-10" />
-            </motion.a>
-          </div>
+            </a>
+          </motion.div>
         </div>
       </div>
     </section>
