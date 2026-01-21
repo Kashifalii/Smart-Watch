950281d8408e1296f13d86cd1ea4b71802343adf
Kashif Ali <136927508+Kashifalii@users.noreply.github.com>
2026-01-21 16:21:55 +0500

    homepage animation applyed

--- Components/swiper/page.tsx
+++ Components/swiper/page.tsx
@@ -1,30 +1,29 @@
 "use client";
+import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
 
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 
-// import required modules
-import { Navigation } from "swiper/modules";
 import HomePage from "../../pages/home";
 
 export default function SwiperComp() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
-    <>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>
-          <HomePage />
-        </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
-    </>
+    <Swiper
+      navigation
+      modules={[Navigation]}
+      onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <HomePage isActive={activeIndex === 0} />
+      </SwiperSlide>
+
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+    </Swiper>
   );
 }
--- pages/home.tsx
+++ pages/home.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import React from "react";
 
 import clock from "@/public/Images/c1.webp";
-import Link from "next/link";
 import { MoveRight } from "lucide-react";
+import { motion } from "framer-motion";
 
-function HomePage() {
+function HomePage({ isActive }: { isActive: boolean }) {
   return (
-    <section className="w-full h-screen bg-[linear-gradient(145deg,rgba(39,39,39,0.7)_50%,rgba(217,70,96,0.94)_50%)]">
+    <section className="w-full h-screen bg-[linear-gradient(145deg,rgba(39,39,39,0.7)_50%,rgba(217,70,96,0.94)_50%)] overflow-hidden">
       <div className="container mx-auto px-4">
         <Image
           alt="Clock Image"
@@ -18,32 +18,81 @@ function HomePage() {
         <div className="h-screen w-full flex items-center">
           {/* left */}
           <div className="w-1/2 h-auto">
-            <p className="text-red uppercase font-sans font-medium text-[26px]">
+            <motion.p
+              initial={{ x: -500, opacity: 0 }}
+              animate={
+                isActive ? { x: 0, opacity: 1 } : { x: -500, opacity: 0 }
+              }
+              transition={{
+                delay: 0.3,
+                duration: 3,
+                type: "spring",
+                stiffness: 50,
+              }}
+              className="text-red uppercase font-sans font-medium text-[26px]"
+            >
               Timeless Precision- Modern Design
-            </p>
-            <h1 className="text-[243px] font-oswald font-semibold text-white leading-48 ps">
+            </motion.p>
+            <motion.h1
+              initial={{ x: -500, opacity: 0 }}
+              animate={
+                isActive ? { x: 0, opacity: 1 } : { x: -500, opacity: 0 }
+              }
+              transition={{ duration: 3, type: "spring", stiffness: 50 }}
+              className="text-[243px] font-oswald font-semibold text-white leading-48 ps"
+            >
               SMART
-            </h1>
-            <h3 className="text-[85px] font-oswald font-extralight text-white">
+            </motion.h1>
+            <motion.h3
+              initial={{ x: -500, opacity: 0 }}
+              animate={
+                isActive ? { x: 0, opacity: 1 } : { x: -500, opacity: 0 }
+              }
+              transition={{
+                delay: 0.2,
+                duration: 3,
+                type: "spring",
+                stiffness: 50,
+              }}
+              className="text-[85px] font-oswald font-extralight text-white"
+            >
               ChronoX - Series
-            </h3>
+            </motion.h3>
           </div>
           {/* right */}
           <div className="w-1/2 h-auto flex flex-col items-end translate-y-12">
-            <h1 className="text-[243px] font-oswald font-semibold text-white leading-48 ps">
+            <motion.h1
+              initial={{ x: 500, opacity: 0 }}
+              animate={isActive ? { x: 0, opacity: 1 } : { x: 500, opacity: 0 }}
+              transition={{ duration: 3, type: "spring", stiffness: 50 }}
+              className="text-[243px] font-oswald font-semibold text-black leading-48 ps"
+            >
               WATCH
-            </h1>
-            <p className="max-w-[451px]  w-full text-xl font-sans font-light text-white mt-8 text-end">
+            </motion.h1>
+            <motion.p
+              initial={{ x: 500, opacity: 0 }}
+              animate={isActive ? { x: 0, opacity: 1 } : { x: 500, opacity: 0 }}
+              transition={{ duration: 3, type: "spring", stiffness: 50 }}
+              className="max-w-[451px]  w-full text-xl font-sans font-light text-white mt-8 text-end"
+            >
               Discover watches crafted for those who value elegance,
               performance, and lasting quality. Designed to elevate every
               moment.
-            </p>
-            <Link
+            </motion.p>
+            <motion.a
+              initial={{ x: 500, opacity: 0 }}
+              animate={isActive ? { x: 0, opacity: 1 } : { x: 500, opacity: 0 }}
+              transition={{
+                delay: 0.2,
+                duration: 3,
+                type: "spring",
+                stiffness: 50,
+              }}
               href={"#"}
               className="text-[42px] font-oswald font-semibold text-white"
             >
               Order Now <MoveRight className="inline size-10" />
-            </Link>
+            </motion.a>
           </div>
         </div>
       </div>
